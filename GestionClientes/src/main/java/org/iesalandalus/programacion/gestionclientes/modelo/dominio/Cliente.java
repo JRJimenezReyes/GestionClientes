c657b1e89d4d90124d6a52aedf500cfacc81fcd0
José Ramón Jiménez Reyes <joseramon.jimenez@iesalandalus.org>
2019-01-08 17:11:46 +0100

    Añado la clase Cliente.

---
+++ GestionClientes/src/main/java/org/iesalandalus/programacion/gestionclientes/modelo/dominio/Cliente.java
@@ -0,0 +1,78 @@
+package org.iesalandalus.programacion.gestionclientes.modelo.dominio;
+
+public class Cliente {
+
+	private DatosPersonales datosPersonales;
+	private DatosContacto datosContacto;
+	
+	public Cliente(DatosPersonales datosPersonales, DatosContacto datosContacto) {
+		setDatosPersonales(datosPersonales);
+		setDatosContacto(datosContacto);
+	}
+	
+	public Cliente(Cliente cliente) {
+		if (cliente == null) {
+			throw new IllegalArgumentException("No se puede copiar un cliente nulo.");
+		}
+		setDatosPersonales(cliente.datosPersonales);
+		setDatosContacto(cliente.datosContacto);
+	}
+	
+	private void setDatosPersonales(DatosPersonales datosPersonales) {
+		if (datosPersonales == null) {
+			throw new IllegalArgumentException("Los datos personales no pueden ser nulos.");
+		}
+		this.datosPersonales = new DatosPersonales(datosPersonales);
+	}
+	
+	public void setDatosContacto(DatosContacto datosContacto) {
+		if (datosContacto == null) {
+			throw new IllegalArgumentException("Los datos de contacto no pueden ser nulos.");
+		}
+		this.datosContacto = new DatosContacto(datosContacto);
+	}
+	
+	public DatosPersonales getDatosPersonales() {
+		return new DatosPersonales(datosPersonales);
+	}
+	
+	public DatosContacto getDatosContacto() {
+		return new DatosContacto(datosContacto);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datosPersonales == null) ? 0 : datosPersonales.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Cliente)) {
+			return false;
+		}
+		Cliente other = (Cliente) obj;
+		if (datosPersonales == null) {
+			if (other.datosPersonales != null) {
+				return false;
+			}
+		} else if (!datosPersonales.equals(other.datosPersonales)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [datos personales=" + datosPersonales + ", datos de contacto=" + datosContacto + "]";
+	}
+	
+}
---
+++ GestionClientes/src/test/java/org/iesalandalus/programacion/gestionclientes/modelo/dominio/ClienteTest.java
@@ -0,0 +1,109 @@
+package org.iesalandalus.programacion.gestionclientes.modelo.dominio;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClienteTest {
+	
+	private static final String ERROR_EXCEPCION = "Debería haber saltado la excepción.";
+	private static final String ERROR_NO_EXCEPCION = "No debería haber saltado la excepción.";
+	private static final DatosPersonales DATOS_PERSONALES = 
+			new DatosPersonales("José Ramón", "Jiménez Reyes", "11223344X");
+	private static final DatosContacto DATOS_CONTACTO =
+			new DatosContacto("950112233", "prueba@iesalandalus.org", 
+					new DireccionPostal("Finca Santa Isabel s/n", "Almería", "04008"));
+
+	@Test
+	public void constructorValidoTest() {
+		try {
+			Cliente cliente = new Cliente(DATOS_PERSONALES, DATOS_CONTACTO);
+			assertEquals(DATOS_PERSONALES, cliente.getDatosPersonales());
+			assertEquals(DATOS_CONTACTO, cliente.getDatosContacto());
+			assertFalse(DATOS_PERSONALES == cliente.getDatosPersonales());
+			assertFalse(DATOS_CONTACTO == cliente.getDatosContacto());
+		} catch (IllegalArgumentException e) {
+			fail(ERROR_NO_EXCEPCION);
+		}
+	}
+	
+	@Test
+	public void constructoDatosPersonalesNoValidoTest() {
+		Cliente cliente = null;
+		try {
+			cliente = new Cliente(null, DATOS_CONTACTO);
+			fail(ERROR_EXCEPCION);
+		} catch (IllegalArgumentException e) {
+			assertNull(cliente);
+			assertEquals("Los datos personales no pueden ser nulos.", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void constructoDatosContactoNoValidoTest() {
+		Cliente cliente = null;
+		try {
+			cliente = new Cliente(DATOS_PERSONALES, null);
+			fail(ERROR_EXCEPCION);
+		} catch (IllegalArgumentException e) {
+			assertNull(cliente);
+			assertEquals("Los datos de contacto no pueden ser nulos.", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void constructorCopiaValidoTest() {
+		try {
+			Cliente cliente1 = new Cliente(DATOS_PERSONALES, DATOS_CONTACTO);
+			Cliente cliente2 = new Cliente(cliente1);
+			assertEquals(DATOS_PERSONALES, cliente2.getDatosPersonales());
+			assertEquals(DATOS_CONTACTO, cliente2.getDatosContacto());
+		} catch (IllegalArgumentException e) {
+			fail(ERROR_NO_EXCEPCION);
+		}
+	}
+	
+	@Test
+	public void constructorCopiaNoValidoTest() {
+		Cliente cliente = null;
+		try {
+			cliente = new Cliente(null);
+			fail(ERROR_EXCEPCION);
+		} catch (IllegalArgumentException e) {
+			assertEquals("No se puede copiar un cliente nulo.", e.getMessage());
+			assertNull(cliente);
+		}
+	}
+	
+	@Test
+	public void equalTest() {
+		Cliente cliente1 = new Cliente(DATOS_PERSONALES, DATOS_CONTACTO);
+		Cliente cliente2 = new Cliente(cliente1);
+		Cliente cliente3 = new Cliente(new DatosPersonales("Paco", "Jones", "22334455X"), DATOS_CONTACTO);
+		assertNotEquals(cliente1, null);
+		assertNotEquals(cliente1, "");
+		assertEquals(cliente1, cliente1);
+		assertEquals(cliente1, cliente2);
+		assertNotEquals(cliente1, cliente3);
+	}
+	
+	@Test
+	public void hashCodeTest() {
+		Cliente cliente1 = new Cliente(DATOS_PERSONALES, DATOS_CONTACTO);
+		Cliente cliente2 = new Cliente(cliente1);
+		Cliente cliente3 = new Cliente(new DatosPersonales("Paco", "Jones", "22334455X"), DATOS_CONTACTO);
+		assertEquals(cliente1.hashCode(), cliente1.hashCode());
+		assertEquals(cliente1.hashCode(), cliente2.hashCode());
+		assertNotEquals(cliente1.hashCode(), cliente3.hashCode());
+	}
+	
+	@Test
+	public void toStringTest() {
+		String clienteStr = "Cliente [datos personales=[nombre=José Ramón, apellidos=Jiménez Reyes, dni=11223344X], "
+				+ "datos de contacto=[teléfono=950112233, correo=prueba@iesalandalus.org, dirección postal="
+				+ "[dirección=Finca Santa Isabel s/n, localidad=Almería, código postal=04008]]]";
+		Cliente cliente = new Cliente(DATOS_PERSONALES, DATOS_CONTACTO);
+		assertEquals(clienteStr, cliente.toString());
+	}
+
+}
