cfc745d636b20f035eb15f45433bd8125c909a00
José Ramón Jiménez Reyes <joseramon.jimenez@iesalandalus.org>
2019-01-08 17:02:54 +0100

    Creo los paquetes para el proyecto.

---
+++ GestionClientes/src/main/java/org/iesalandalus/programacion/gestionclientes/MainApp.java
@@ -0,0 +1,9 @@
+package org.iesalandalus.programacion.gestionclientes;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		System.out.println("Programa para la gestión de clientes");
+	}
+
+}
