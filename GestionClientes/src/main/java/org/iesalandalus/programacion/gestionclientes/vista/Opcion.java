5dbe4a6dbbf39945deb61b2f45108f0a0c2d7abc
José Ramón Jiménez Reyes <joseramon.jimenez@iesalandalus.org>
2019-01-08 17:17:22 +0100

    Añado el enumerado Opcion para la gestión del menú.

---
+++ GestionClientes/src/main/java/org/iesalandalus/programacion/gestionclientes/vista/Opcion.java
@@ -0,0 +1,62 @@
+package org.iesalandalus.programacion.gestionclientes.vista;
+
+public enum Opcion {
+	SALIR("Salir") {
+		public void ejecutar() {
+			vista.salir();
+		}
+	},
+	INSERTAR_AULA("Insertar cliente") {
+		public void ejecutar() {
+			vista.insertarCliente();
+		}
+	},
+	BORRAR_AULA("Borrar cliente") {
+		public void ejecutar() {
+			vista.borrarCliente();
+		}
+	},
+	BUSCAR_AULA("Buscar cliente") {
+		public void ejecutar() {
+			vista.buscarCliente();
+		}
+	},
+	LISTAR_AULAS("Listar clientes") {
+		public void ejecutar() {
+			vista.listarClientes();
+		}
+	};
+	
+	private String mensajeAMostrar;
+	private static IUTextual vista;
+	
+	private Opcion(String mensajeAMostrar) {
+		this.mensajeAMostrar = mensajeAMostrar;
+	}
+	
+	public String getMensaje() {
+		return mensajeAMostrar;
+	}
+	
+	public abstract void ejecutar();
+	
+	protected static void setVista(IUTextual vista) {
+		Opcion.vista = vista;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%d.- %s", ordinal(), mensajeAMostrar);
+	}
+	
+	public static Opcion getOpcionSegunOrdinal(int ordinal) {
+		if (esOrdinalValido(ordinal))
+			return values()[ordinal];
+		else
+			throw new IllegalArgumentException("Ordinal de la opción no válido");
+	}
+	
+	public static boolean esOrdinalValido(int ordinal) {
+		return (ordinal >= 0 && ordinal <= values().length - 1);
+	}
+}
