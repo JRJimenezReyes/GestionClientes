65062497f5a6bc1fba191be91fff66693b03d8e6
José Ramón Jiménez Reyes <joseramon.jimenez@iesalandalus.org>
2019-01-08 17:20:09 +0100

    Modifico la clase MainApp para que ejecute la IU Textual.

--- GestionClientes/src/main/java/org/iesalandalus/programacion/gestionclientes/MainApp.java
+++ GestionClientes/src/main/java/org/iesalandalus/programacion/gestionclientes/MainApp.java
@@ -1,9 +1,13 @@
 package org.iesalandalus.programacion.gestionclientes;
 
+import org.iesalandalus.programacion.gestionclientes.vista.IUTextual;
+
 public class MainApp {
 
 	public static void main(String[] args) {
 		System.out.println("Programa para la gestión de clientes");
+		IUTextual vista = new IUTextual();
+		vista.comenzar();
 	}
 
 }
